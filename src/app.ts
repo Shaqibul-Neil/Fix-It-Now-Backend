d33444296f20db5d8ee5e2f6352e9ce52f3d8d90
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-07-05 20:43:33 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import type { TApplication } from "./types/express.types";
+
+const app: TApplication = express();
+
+export default app;
---
+++ src/types/express.types.ts
@@ -0,0 +1,13 @@
+import {
+  type Application,
+  type NextFunction,
+  type Request,
+  type Response,
+  type RequestHandler,
+} from "express";
+
+export type TRequest = Request;
+export type TResponse = Response;
+export type TApplication = Application;
+export type TNextFunction = NextFunction;
+export type TRequestHandler = RequestHandler;
